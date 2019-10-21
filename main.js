a29fe9d384a28ee152a89a8b9fab60356085d224
ammaralsaeed123 <55942208+ammaralsaeed123@users.noreply.github.com>
2019-10-21 09:54:09 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,26 +4,45 @@ let dimg = document.getElementById("dimg");
 let close = document.getElementById("close");
 let next = document.getElementById("next");
 let pre = document.getElementById("pre");
-for(let i = 0 ; i<=6 ; i++){
-show[i].addEventListener("click" , getAqwe);
+let hamza = ["screen-9.jpg", "151564065774128.jpg", "nhbnhb-12.jpg", "pho4.png", "nhbnhb-5.jpg", "nhbnhb-3.jpg"];
+let r = 0;
+for (let i = 0; i <= 6; i++) {
+    show[i].addEventListener("click", getAqwe);
 }
+
 function getAqwe(event) {
-   all12.src = event.target.src;
+    all12.src = event.target.src;
     dimg.style.display = "block";
 }
 
 
-close.addEventListener("click" , closePhoto);
+close.addEventListener("click", closePhoto);
+
 function closePhoto() {
     dimg.style.display = "none";
 }
 
-next.addEventListener("click",nextPhoto);
+
+
 function nextPhoto() {
-    let hamza = ["Cy2f0r5XcAAahFv.jpg","mujib.jpg","nature-3082832_960_720.jpg","pexels-photo-814499.jpeg","travel1.531473.jpg","waterfall.jpg"];
-    let num = 0;
-    all12.setAttribute("src" , hamza[num]);
-    num++;
- 
-    
+
+    if (r >= hamza.length - 1) {
+
+    } else {
+        all12.setAttribute("src", hamza[r++]);
+    }
+
+
+
+}
+
+function prePhoto() {
+
+
+
+    if (r == 0) {
+
+    } else {
+        all12.setAttribute("src", hamza[r--]);
+    }
 }
